998faa28a623711925a37debb1fff32742f39555
Mikael Boucher <mikaelboucher@gmail.com>
2017-11-15 23:29:20 -0500

    Major updates
    Missing the log out procedure and fine tuning.
    MB

--- Client/TPClient/TPClient/Main.cpp
+++ Client/TPClient/TPClient/Main.cpp
@@ -1,10 +1,17 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include <winsock2.h>	//Most of the Winsock functions, structures, and definitions
 #include <ws2tcpip.h>	//Definitions introduced in the WinSock 2 Protocol-Specific Annex document for TCP/IP
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <regex>
+#include <regex>		
+#include <conio.h>		//_gretch() : press any key to continue
+#include <thread>		
 
+/*
+TO DO : PICK AND IMPLEMENT LOG OUT PROCEDURE (empty message as suggested in email?
+*/
 
 using namespace std;
 
@@ -14,120 +21,215 @@ using namespace std;
 #define IPV4_LENGTH 16
 #define PORT_MAX_LENGTH 5
 
+#define MESSAGE_MAX_LENGTH 200
+#define MAX_MSG_BUF_LENGTH 300	//TO DO : Validate with Salim and calculate real max
+#define MAX_USERNAME_LENGTH 10	//TO DO : Validate with Salim
+#define MAX_PASSWORD_LENGTH 8	//TO DO : Validate with Salim
+
+//Headers and separator
+const string MSG_TYPE_MSG = "$msg:";
+const string MSG_TYPE_LOG = "$log:";
+const string MSG_TYPE_LOGIN_OK = "$login_ok:";
+const string MSG_TYPE_LOGIN_FAILED = "$login_failed:";
+const string MSG_TYPE_LOGOUT = "$quit:";
+const string DATA_SEPARATOR = "%__%";
+
 /* Function prototyping */
+void requestPortAndIP(Client &client);
+void requestLoginInfo(Client &client);
+bool validateUsername(string &username);
+bool validatePassword(string &password);
+bool validateIP(string &ip);
+bool validatePort(string &port);
+int submitLoginRequest(Client &client);
+int processIncomingMessage(Client &client);
+void printMessage(Client &client);
 
-void requestPortAndIP(string* ipAddress, string* &port);
-bool validateIP(string* ip);
-bool validatePort(string* port);
-
+struct Client
+{
+	SOCKET socket;
+	char receivedMessage[MAX_MSG_BUF_LENGTH];
+	string ipAddress;
+	string port;
+	string username;
+	string password;
+};
 
 int main() {
 
-	/* Set up winsock - This part is exactly the same for client and server */
+	// Set up winsock - This part is exactly the same for client and server */
 
 	WSADATA wsaData;	//Structure that contains information about the Windows Sockets implementation
 
 	int iResult;
 
-	// Initialize Winsock
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); //MAKEWORD(2,2) Request for version 2.2 of Winsock on the system
 	if (iResult != 0) {
 		printf("WSAStartup failed: %d\n", iResult);
+		printf("Press any key to end the program\n");
+		_getch();
 		return 1;
 	}
 
-	//  addringo : Object that contains a sockaddr structure
+	//  addrinfo : Object that contains a sockaddr structure
 	struct addrinfo *result = NULL,
 		*ptr = NULL,
 		hints;
 
 	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;		//internet address family (CHANGE TO AF_INET ??)
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
 
-	string* ipAddress = new string;
-	string* port = new string;
+	Client client = { INVALID_SOCKET, "", "", "", "", ""};
 
-	requestPortAndIP(ipAddress, port);
+	requestPortAndIP(client);
+	requestLoginInfo(client);
+
+	string ip = client.ipAddress;
 
 	// Resolve the server address and port
-	/*
-	iResult = getaddrinfo(ipAddress, port, &hints, &result);
+	iResult = getaddrinfo((PCSTR)&client.ipAddress, (PCSTR)&client.port, &hints, &result);	//TODO if connection issues, perhaps casts don't work
 	if (iResult != 0) {
 		printf("getaddrinfo failed: %d\n", iResult);
 		WSACleanup();
+		printf("Press any key to end the program\n");
+		_getch();
 		return 1;
 	}
-	*/
-	// TO CONTINUE
 
-	delete ipAddress;
-	delete port;
+	//Find first valid address
+	while ((result != NULL) && (result->ai_family != AF_INET))
+		result = result->ai_next;
+	
+	//If address not found
+	if (((result->ai_family != AF_INET || (result == NULL)))) {
+		freeaddrinfo(result);
+		WSACleanup();
+		printf("Cannot find address \n");
+		printf("Press any key to end the program\n");
+		_getch();
+		return 1;
+	}
+
+	sockaddr_in *address;
+	address = (struct sockaddr_in *) result->ai_addr;
+
+	printf("Connecting to port %s of server %s\n", client.port, inet_ntoa(address->sin_addr));
+
+	ptr = result;
+
+	// Create a SOCKET for connecting to server
+	client.socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+	if (client.socket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		printf("Press any key to end the program\n");
+		_getch();
+		return 1;
+	}
+
+	//Connecting to server
+	iResult = connect(client.socket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		closesocket(client.socket);
+		client.socket = INVALID_SOCKET;
+		printf("Cannot connect to port %s of server %s\n", client.port, inet_ntoa(address->sin_addr));
+		freeaddrinfo(result);	// TODO : Should this be done once out of if?
+		WSACleanup();
+		printf("Appuyez une touche pour finir\n");
+		getchar();
+		return 1;
+	}
+
+	cout << "Successful connection so server" << endl;
+
+	if (!submitLoginRequest(client)) {
+		closesocket(client.socket);
+		client.socket = INVALID_SOCKET;
+		printf("Login failed\n", client.port, inet_ntoa(address->sin_addr));
+		freeaddrinfo(result);
+		WSACleanup();
+		printf("Appuyez une touche pour finir\n");
+		getchar();
+		return 1;
+	}
+
+	//thread listening to server
+	thread my_thread(processIncomingMessage, client);
+	string messageToServer = "";
+
+	while (1)
+	{
+		getline(cin, messageToServer);
+		iResult = send(client.socket, messageToServer.c_str(), strlen(messageToServer.c_str()), 0);
+
+		if (iResult <= 0)
+		{
+			cout << "send() failed: " << WSAGetLastError() << endl;
+			break;
+		}
+	}
+
+	//Shutdown the connection since no more data will be sent
+	my_thread.detach();
 
 	return 0;
 }
 
-void requestPortAndIP(string* ipAddress, string* &port) {
+void requestPortAndIP(Client& client) {
 
-	cout << "Launching chatroom" << endl;
-
-	//char tempIP[IPV4_LENGTH];
+	cout << "Launching chatroom software" << endl;
 
 	do {
 		cout << "Please enter the server IP address: ";
-		cin >> *ipAddress;
-	} while (!validateIP(ipAddress));
+		cin >> client.ipAddress;
+	} while (!validateIP(client.ipAddress));
 	
 	do {
-		cout << "Please pick a port number between 10000 and 10050: ";
-		cin >> *port;
-	} while (!validatePort(port));
+		cout << "Please pick a port number between 5000 and 5050: ";
+		cin >> client.port;
+	} while (!validatePort(client.port));
+}
 
-	cout << "Port ok";
+void requestLoginInfo(Client &client) {
 
-	while (1);
+	do {
+		cout << "Please enter your username: ";
+		cin >> client.username;
+	} while (!validateUsername(client.username));
 
-	// TO CONTINUE
-	/*
-	void saisirParametres(char*& adresseIP, char*& port) {
-
-		cout << "Parametres du serveur a joindre" << endl;
-
-		char adresseIPTemp[LONGUEUR_ADRESSE_IP];
-		//demander l'adresse IP tant qu'elle n'est pas valide
-		do {
-			cout << endl << "Entrer l'adresse IP du poste du serveur a joindre: ";
-			gets_s(adresseIPTemp);
-		} while (!estFormatIP(adresseIPTemp));
-
-		//enregistrer l'adresse IP validée
-		for (size_t i = 0; i < sizeof(adresseIPTemp); i++) {
-			adresseIP[i] = adresseIPTemp[i];
-		}
-
-		char portTemp[LONGUEUR_PORT];
-		//demander le port tant qu'il n'est pas valide
-		do {
-			cout << endl << "Entrer le port d'ecoute (entre 6000 et 6050): ";
-			gets_s(portTemp);
-		} while (!estPortValide(portTemp));
-
-		//enregistrer le port validé
-		for (size_t i = 0; i < sizeof(portTemp); i++) {
-			port[i] = portTemp[i];
-		}
-
-	}
-	*/
+	do {
+		cout << "Please enter your password: ";
+		cin >> client.password;
+	} while (!validatePassword(client.password));
 
 }
 
-bool validateIP(string* ip) {
+bool validateUsername(string &username) {
+	int length = username.length();
+	if (length > MAX_USERNAME_LENGTH){
+		cout << "Username must be below " << MAX_USERNAME_LENGTH << " characters." << endl;
+		return false;
+	}
+	return true;
+};
 
-	regex ipRegex("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+bool validatePassword(string &password) {
+	if (password.length() > MAX_PASSWORD_LENGTH) {
+		cout << "Password must be below " << MAX_PASSWORD_LENGTH << " characters." << endl;
+		return false;
+	}
+	return true;
+};
 
-	bool isValidIp = regex_search(*ip, ipRegex);
+bool validateIP(string& ip) {
+
+	regex ipRegex("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+
+	bool isValidIp = regex_search(ip, ipRegex);
 
 	if (!isValidIp) {
 		cout << "The IP address is not valid. Please enter a valid IP adress." << endl;
@@ -136,15 +238,70 @@ bool validateIP(string* ip) {
 	return (isValidIp);
 }
 
-bool validatePort(string* port) {
+bool validatePort(string& port) {
 
-	regex ipRegex("^100([0-4][0-9]|50)$");
+	regex ipRegex("^50([0-4][0-9]|50)$");
 
-	bool isValidIp = regex_search(*port, ipRegex);
+	bool isValidPort = regex_search(port, ipRegex);
 
-	if (!isValidIp) {
-		cout << "The port number is not valid. Please enter a valid port number between 10000 and 100050." << endl;
+	if (!isValidPort) {
+		cout << "The port number is not valid. Please enter a valid port number between 5000 and 5050." << endl;
 	}
 
-	return (regex_search(*port, ipRegex));
+	return (isValidPort);
 }
+
+int submitLoginRequest(Client &client) {
+	
+	int iResult;
+	string loginInfo = MSG_TYPE_LOG + client.username + DATA_SEPARATOR + client.password;
+	//Send login info
+	iResult = send(client.socket, loginInfo.c_str(), strlen(loginInfo.c_str()), 0);
+	//Analyse login response
+	recv(client.socket, client.receivedMessage, MAX_MSG_BUF_LENGTH, 0);
+	string messageFromServer = client.receivedMessage;
+
+	if (messageFromServer == MSG_TYPE_LOGIN_FAILED)
+		requestLoginInfo(client);
+	else if (messageFromServer == MSG_TYPE_LOGIN_OK)
+		return 1;
+	return 0;
+}
+
+void printMessage(Client& client) {
+	string receivedMessage = client.receivedMessage;
+	if (receivedMessage.substr(0, MSG_TYPE_MSG.length()) == MSG_TYPE_MSG) {
+		receivedMessage.erase(0, MSG_TYPE_MSG.length());
+		cout << "MESSAGE DATA : " << receivedMessage << endl;
+	}
+	else
+		cout << "UNKNWON MESSAGE HEADER" << endl;
+};
+
+
+int processIncomingMessage(Client& client)
+{
+	while (1)
+	{
+		if (client.socket != 0)
+		{
+			int iResult = recv(client.socket, client.receivedMessage, MAX_MSG_BUF_LENGTH, 0);
+
+			if (iResult != SOCKET_ERROR)
+				printMessage(client);
+			else
+			{
+				cout << "recv() failed: " << WSAGetLastError() << endl;
+				break;
+			}
+		}
+	}
+
+	if (WSAGetLastError() == WSAECONNRESET)
+		cout << "The server has disconnected" << endl;
+
+	return 0;
+}
+
+
+
